0ee091914b806c938538190a0502576b9d7361ee
DomingosCesar <147095145+DomingosCesar@users.noreply.github.com>
2025-11-23 12:18:37 +0100

    doing authentication functionality

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Core.index, name='index'),
+    # Authentication routes
+    path('auth/login/', views.Auth.login, name='login'),
+    path('auth/register/', views.Auth.register, name='register'),
+    path('auth/register/store', views.Auth.store, name='store'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 
 
 class Core:
     def index(request):
         return render(request, 'index.html')
+
+class Auth:
+    def register(request):
+        name = 'register'
+        return render(request, 'auth/auth_form.html', {'name':name})
+    
+    def store(request):
+        return redirect('index')
+    
+    def login(request):
+        name = 'login'
+        return render(request, 'auth/auth_form.html', {'name':name})
